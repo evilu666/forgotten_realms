af1dfbfb31689e0b9e43c667a9f13248a0519944
evilu666 <evilulu@proton.me>
2022-07-17 02:27:24 +0200

    Fixed loading issue

---
+++ src/main/java/com/evilu/forgottenRealms/registry/BiomeRegistry.java
@@ -0,0 +1,24 @@
+package com.evilu.forgottenRealms.registry;
+
+import com.evilu.forgottenRealms.ForgottenRealmsMod;
+
+import net.minecraft.core.Registry;
+import net.minecraft.data.BuiltinRegistries;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.biome.Biome;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+/**
+ * BiomeRegistry
+ */
+public class BiomeRegistry {
+
+    private static final DeferredRegister<Biome> BIOMES = DeferredRegister.create(ForgeRegistries.BIOMES, ForgottenRealmsMod.MODID);
+
+
+
+    
+}
