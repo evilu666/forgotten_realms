28d5f9b2158050f4764399965d15ec7d92cb1584
Lucas Friese <friese@tembit.de>
2022-07-18 17:39:35 +0200

    Curios integration

--- src/main/java/com/evilu/forgottenRealms/ForgottenRealmsMod.java
+++ src/main/java/com/evilu/forgottenRealms/ForgottenRealmsMod.java
@@ -8,6 +8,7 @@ import com.evilu.forgottenRealms.registry.ItemRegistry;
 
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
@@ -15,6 +16,8 @@ import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import top.theillusivec4.curios.api.SlotTypeMessage;
+import top.theillusivec4.curios.api.SlotTypePreset;
 
 @Mod(ForgottenRealmsMod.MODID)
 public class ForgottenRealmsMod
@@ -42,9 +45,13 @@ public class ForgottenRealmsMod
     private void clientSetup(final FMLClientSetupEvent event) {
     }
 
-    private void enqueueIMC(final InterModEnqueueEvent event) {}
+    private void enqueueIMC(final InterModEnqueueEvent event) {
+        // Set default curios slot amounts
+        event.enqueueWork(() -> InterModComms.sendTo("curios", SlotTypeMessage.REGISTER_TYPE, SlotTypePreset.RING.getMessageBuilder()::build));
+    }
 
-    private void processIMC(final InterModProcessEvent event) { }
+    private void processIMC(final InterModProcessEvent event) {
+    }
 
     @SubscribeEvent
     public void onServerStarting(final ServerStartingEvent event) {}
--- src/main/java/com/evilu/forgottenRealms/registry/ItemRegistry.java
+++ src/main/java/com/evilu/forgottenRealms/registry/ItemRegistry.java
@@ -1,6 +1,12 @@
 package com.evilu.forgottenRealms.registry;
 
+import java.util.Map;
+
 import com.evilu.forgottenRealms.ForgottenRealmsMod;
+import com.evilu.forgottenRealms.item.BaseEffectRingItem;
+
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.DoubleHighBlockItem;
 import net.minecraft.world.item.Item;
@@ -9,11 +15,15 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
-
 public class ItemRegistry {
 
     private static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, ForgottenRealmsMod.MODID);
 
+    public static final RegistryObject<Item> FIRE_AMULET = ITEMS.register("fire_amulet", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_COMBAT)));
+    public static final RegistryObject<Item> WATER_AMULET = ITEMS.register("water_amulet", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_COMBAT)));
+    public static final RegistryObject<Item> MING_RING = ITEMS.register("mining_ring", () -> new BaseEffectRingItem(new Item.Properties().tab(CreativeModeTab.TAB_COMBAT), Map.of(
+        MobEffects.DIG_SPEED, effectInstance -> new MobEffectInstance(effectInstance.getEffect(), effectInstance.getDuration(), 3)
+    )));
 
     public static void registerItems() {
         ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
